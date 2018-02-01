0fc3ef73d92ebda416d3b54f6c53c0abe3b072cb
Austin Stevens <austinestevens@outlook.com>
2018-02-01 16:53:48 -0500

    Added ability to edit beer names of existing beers in the beer list. Beers can also be removed from the list.

--- views/BeerView.js
+++ views/BeerView.js
@@ -5,8 +5,10 @@ var BeerView = Backbone.View.extend({
   template: Handlebars.compile($('#beer-template').html()),
 
   events: {
-    'click .beer-name': 'makeEditable'
-  }
+    'click .edit': 'makeEditable',
+    'keypress input.beer-name': 'makeStatic',
+    'click .remove': 'removeBeer'
+  },
 
   render: function () {
     this.$el.html(this.template(this.model.toJSON()));
@@ -15,7 +17,33 @@ var BeerView = Backbone.View.extend({
   },
 
   makeEditable: function () {
-    this.$el.find('.beer-name').
-  }
+    // Hide our p element
+    this.$('p').first().addClass('edit-mode');
+    // Show our input with the beer name
+    this.$('input').removeClass('edit-mode');
+    // Focus on the input
+    this.$('input').focus();
+  },
 
+  makeStatic: function (event) {
+    // Only end editing if enter is pressed
+    if (event.keyCode === 13) {
+      var $value = this.$('input').val().trim();
+
+      // Update the model with the new beer name
+      this.model.set('name', $value);
+      // Update the view with the new beer name
+      this.$('p').first().html($value);
+      // Remove editing capability by hiding the input
+      this.$('p[class="beer-name edit-mode"]').removeClass('edit-mode');
+      this.$('input').addClass('edit-mode');
+    }
+  },
+
+  removeBeer: function () {
+    // Remove the model of the beer
+    this.model.destroy();
+    this.$el.remove();
+
+  }
 });
